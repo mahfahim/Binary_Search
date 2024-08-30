f8139ffcf162ca2216e9c6f541fab2074b3ad9ac
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-08-30 12:26:49 +0600

    Create 1_Search_Insert_Position.cpp

---
+++ 1_Search_Insert_Position.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int ans;
+        bool flag = true;
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            if(nums[mid]==target)
+            {
+              ans = mid;
+              flag = false;
+              break;   
+            }
+            else if( nums[mid]<target) l=mid+1;
+            else if( nums[mid]>target) r=mid-1;
+        }
+        if(flag)
+        {
+           ans = max(l,r);
+        }
+        return ans ;
+    }
+    
+};
